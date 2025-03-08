bd68b7946a38ff7f4b016cd7f1b37df8968393bf
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:08:57 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/funding/index.ts

---
+++ src/web/src/pages/funding/index.ts
@@ -0,0 +1,13 @@
+// Import React
+import React from 'react';
+
+// Import the FundingList component for re-export
+import FundingList from './FundingList';
+// Import the FundingDetail component for re-export
+import FundingDetail from './FundingDetail';
+
+// Export the FundingList component for use in routing and other parts of the application
+export { FundingList };
+
+// Export the FundingDetail component for use in routing and other parts of the application
+export { FundingDetail };
