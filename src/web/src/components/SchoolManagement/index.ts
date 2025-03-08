d4a1dc9be14aa9144b96db2b417917c05f5c5003
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:06:20 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/components/SchoolManagement/index.ts

---
+++ src/web/src/components/SchoolManagement/index.ts
@@ -0,0 +1,16 @@
+// Barrel file that exports all school management related components
+// This provides a centralized import point for these components throughout the application.
+
+// Importing SchoolForm component
+import SchoolForm from './SchoolForm';
+// Importing ProgramForm component
+import ProgramForm from './ProgramForm';
+// Importing SchoolAdminForm component
+import SchoolAdminForm from './SchoolAdminForm';
+
+// Exporting SchoolForm component
+export { SchoolForm };
+// Exporting ProgramForm component
+export { ProgramForm };
+// Exporting SchoolAdminForm component
+export { SchoolAdminForm };
