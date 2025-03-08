9ecd5a403b639d3e0f3ce52819c2fe3d30405d0e
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:09:49 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/users/index.ts

---
+++ src/web/src/pages/users/index.ts
@@ -0,0 +1,32 @@
+// Import UserList component from its file
+import UserList from './UserList';
+// Import UserDetail component from its file
+import UserDetail from './UserDetail';
+// Import UserEdit component from its file
+import UserEdit from './UserEdit';
+// Import UserNew component from its file
+import UserNew from './UserNew';
+
+/**
+ * Exports the UserList component for use in other modules
+ * @component
+ */
+export { UserList };
+
+/**
+ * Exports the UserDetail component for use in other modules
+ * @component
+ */
+export { UserDetail };
+
+/**
+ * Exports the UserEdit component for use in other modules
+ * @component
+ */
+export { UserEdit };
+
+/**
+ * Exports the UserNew component for use in other modules
+ * @component
+ */
+export { UserNew };
