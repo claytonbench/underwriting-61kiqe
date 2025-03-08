c86479d0d11c3ed030e8b181294e35854cbdd2ba
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:09:11 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/reports/index.ts

---
+++ src/web/src/pages/reports/index.ts
@@ -0,0 +1,21 @@
+// Import the ApplicationVolumeReport component
+import ApplicationVolumeReport from './ApplicationVolumeReport';
+// Import the DocumentStatusReport component
+import DocumentStatusReport from './DocumentStatusReport';
+// Import the FundingReport component
+import FundingReport from './FundingReport';
+// Import the UnderwritingReport component
+import UnderwritingReport from './UnderwritingReport';
+// Import the ReportDashboard component
+import ReportDashboard from './ReportDashboard';
+
+// Export the ApplicationVolumeReport component for use in the application
+export { ApplicationVolumeReport };
+// Export the DocumentStatusReport component for use in the application
+export { DocumentStatusReport };
+// Export the FundingReport component for use in the application
+export { FundingReport };
+// Export the UnderwritingReport component for use in the application
+export { UnderwritingReport };
+// Export the ReportDashboard component for use in the application
+export { ReportDashboard };
