bd45b8cb867b111a4e55e6a1e9ac1f6ee72e615f
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:07:07 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/components/common/FileUpload/index.ts

---
+++ src/web/src/components/common/FileUpload/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Barrel file for the FileUpload component.
+ * 
+ * This file exports the FileUpload component from the FileUpload directory,
+ * allowing for cleaner imports throughout the application.
+ * Instead of importing from the specific file path:
+ * import FileUpload from 'components/common/FileUpload/FileUpload';
+ * 
+ * Components can use the simplified import:
+ * import FileUpload from 'components/common/FileUpload';
+ */
+
+import FileUpload from './FileUpload';
+
+export default FileUpload;
