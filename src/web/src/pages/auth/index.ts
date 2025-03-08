b7a454562efcd17327ad93a6db1e973d260b0338
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:08:31 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/auth/index.ts

---
+++ src/web/src/pages/auth/index.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Internal imports
+import Login from './Login';
+import ForgotPassword from './ForgotPassword';
+import ResetPassword from './ResetPassword';
+
+/**
+ * Exports the Login page component for use in routing.
+ * @component
+ * @type {React.FC}
+ */
+export { Login };
+
+/**
+ * Exports the ForgotPassword page component for use in routing.
+ * @component
+ * @type {React.FC}
+ */
+export { ForgotPassword };
+
+/**
+ * Exports the ResetPassword page component for use in routing.
+ * @component
+ * @type {React.FC}
+ */
+export { ResetPassword };
