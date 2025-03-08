bf324ec58e20cf78b47a81cb1f1bdf0f5ee9b83d
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:09:41 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/underwriting/index.ts

---
+++ src/web/src/pages/underwriting/index.ts
@@ -0,0 +1,22 @@
+// Import necessary components from the current directory
+import ApplicationReview from './ApplicationReview';
+import UnderwriterDashboard from './UnderwriterDashboard';
+import UnderwritingQueue from './UnderwritingQueue';
+
+/**
+ * Exports the ApplicationReview component for use in routing.
+ * @component
+ */
+export { ApplicationReview };
+
+/**
+ * Exports the UnderwriterDashboard component for use in routing.
+ * @component
+ */
+export { UnderwriterDashboard };
+
+/**
+ * Exports the UnderwritingQueue component for use in routing.
+ * @component
+ */
+export { UnderwritingQueue };
