d82acfa478a90add46f8dffc20e007061d3a6cc2
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:06:40 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/components/common/Breadcrumbs/index.ts

---
+++ src/web/src/components/common/Breadcrumbs/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Barrel file for the Breadcrumbs component
+ * 
+ * This file provides a clean import interface for the Breadcrumbs component
+ * throughout the application, addressing the Component Organization and
+ * Navigation Consistency requirements from the technical specifications.
+ */
+
+import Breadcrumbs from './Breadcrumbs';
+
+export default Breadcrumbs;
