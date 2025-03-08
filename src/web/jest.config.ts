018e4cd4bd3636055ec5078e47bdec7b2853549c
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:04:05 -0700

    Add public-samples/underwriting-61kiqe/src/web/jest.config.ts

---
+++ src/web/jest.config.ts
@@ -0,0 +1,68 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@layouts/(.*)$': '<rootDir>/src/layouts/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@responsive/(.*)$': '<rootDir>/src/responsive/$1',
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.tsx',
+    '!src/serviceWorker.ts',
+    '!src/reportWebVitals.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/__mocks__/**',
+    '!src/test-utils/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coverageReporters: ['text', 'lcov', 'html'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/', '/coverage/'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
+  resetMocks: true,
+  clearMocks: true,
+  verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+      isolatedModules: true
+    }
+  }
+};
+
+export default config;
