d2271b80a7ed7c267304f1d4902f415f3c565582
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:08:46 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/documents/index.ts

---
+++ src/web/src/pages/documents/index.ts
@@ -0,0 +1,22 @@
+// src/web/src/pages/documents/index.ts
+import DocumentList from './DocumentList';
+import DocumentDetail from './DocumentDetail';
+import DocumentSign from './DocumentSign';
+
+/**
+ * Exports the DocumentList component for use in routing.
+ * @component
+ */
+export { DocumentList };
+
+/**
+ * Exports the DocumentDetail component for use in routing.
+ * @component
+ */
+export { DocumentDetail };
+
+/**
+ * Exports the DocumentSign component for use in routing.
+ * @component
+ */
+export { DocumentSign };
