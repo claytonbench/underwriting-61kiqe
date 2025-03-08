a62f569ef9b586944d77a5b0196d762d7733d5b8
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:08:25 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/application/index.ts

---
+++ src/web/src/pages/application/index.ts
@@ -0,0 +1,18 @@
+import ApplicationList from './ApplicationList';
+import ApplicationDetail from './ApplicationDetail';
+import ApplicationNew from './ApplicationNew';
+
+/**
+ * Exports the application list component
+ */
+export { ApplicationList };
+
+/**
+ * Exports the application detail component
+ */
+export { ApplicationDetail };
+
+/**
+ * Exports the application creation component
+ */
+export { ApplicationNew };
