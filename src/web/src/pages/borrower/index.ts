609050c536258781af1fdf2139b65fb21e47a757
Clayton Bench <clayton@benchfamily.org>
2025-03-08 13:08:37 -0700

    Add public-samples/underwriting-61kiqe/src/web/src/pages/borrower/index.ts

---
+++ src/web/src/pages/borrower/index.ts
@@ -0,0 +1,27 @@
+// Import the BorrowerDashboard component for the borrower's main dashboard page
+import BorrowerDashboard from './BorrowerDashboard';
+// Import the BorrowerDocuments component for the borrower's document management page
+import BorrowerDocuments from './BorrowerDocuments';
+// Import the BorrowerProfile component for the borrower's profile management page
+import BorrowerProfile from './BorrowerProfile';
+
+/**
+ * Export the BorrowerDashboard component for use in routing
+ * @component
+ * @exports BorrowerDashboard
+ */
+export { BorrowerDashboard };
+
+/**
+ * Export the BorrowerDocuments component for use in routing
+ * @component
+ * @exports BorrowerDocuments
+ */
+export { BorrowerDocuments };
+
+/**
+ * Export the BorrowerProfile component for use in routing
+ * @component
+ * @exports BorrowerProfile
+ */
+export { BorrowerProfile };
